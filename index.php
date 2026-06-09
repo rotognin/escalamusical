767fa2f96e1ea1b1dd8a5912761c2a3dad2415f0
Rodrigo Tognin <rotog@outlook.com>
2026-06-09 13:30:21 -0300

    Ajuste na margem do título do grupo

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ array_shift($meses);
                 $info_grupo = '';
 
                 if ($grupo['gruObservacoes'] != '') {
-                    $info_grupo = '<span class="w3-margin-left">' . $grupo['gruObservacoes'] . '</span>';
+                    $info_grupo = '<span class="w3-tag w3-black">Obs:</span><span class="w3-margin-left">' . $grupo['gruObservacoes'] . '</span>';
                 }
 
                 echo '<div class="w3-container w3-white w3-margin-top">';
